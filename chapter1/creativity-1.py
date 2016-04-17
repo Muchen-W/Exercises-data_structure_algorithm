97517376c425cf12d5ce54270fba560a7dacf44c
MuchenWu <muchen.w@gmail.com>
2016-04-17 16:09:18 -0700

    finish creativity problems in chapter 1

--- chapter1/creativity-1.py
+++ chapter1/creativity-1.py
@@ -18,6 +18,7 @@ def pair_product_odd(data):
 def isDistinct(data):    # slow
     return len(data) == len(set(data))
 
+
 def is_distinct(data):    # fast
     result = dict()
     for item in data:
@@ -51,6 +52,60 @@ def reverseLines():
         print(item)
 
 
+def dotProduct(arrayA, arrayB):
+    n = len(arrayA)
+    arrayC = list()
+    if not (len(arrayB) == n):
+        raise TypeError('Two arrays must be same length')
+    else:
+        arrayC = [arrayA[i]*arrayB[i] for i in range(n)]
+        return arrayC
+
+
+def writeElement(lst, elemt, index):
+    try:
+        lst[index] = elemt
+    except IndexError:
+        print('Don’t try buffer overflow attacks in Python!')
+    return lst
+
+
+def countVowels1(str):
+    vowels = dict({'a':0, 'e':0, 'i':0, 'o':0, 'u':0})
+    for item in str:
+        if item in vowels:
+            vowels[item] += 1
+        else:
+            vowels[item] = 1
+    return vowels['a']+vowels['e']+vowels['i']+vowels['o']+vowels['u']
+
+
+def countVowels2(str):
+    cnt = 0
+    for item in str:
+        if item in 'aeiou':
+            cnt += 1
+    return cnt
+
+# c1-27
+def factors(n): # generator that computes factors
+    k = 1
+    rev_factor = list()
+    while k*k < n: # while k < sqrt(n)
+        if n % k == 0:
+            yield k
+            rev_factor.append(n//k)
+        k += 1
+    if k*k == n: # special case if n is perfect square
+        yield k
+    for item in rev_factor[::-1]:
+        yield item
+
+
+# c1-28
+def norm(v, p=2):
+    return sum([vi**p for vi in v])**(1/p)
+
 
 if __name__ == '__main__':
     import math
@@ -72,3 +127,5 @@ if __name__ == '__main__':
     print(data)
     print(shuffle(data))
     reverseLines()
+
+    print('dot product:', dotProduct(data, reverseList(data)))
